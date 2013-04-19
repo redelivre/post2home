c430cc8f43c679ab0c48094585ce0d49cc70b006
Eduardo Zulian <eduardozulian@gmail.com>
2013-04-19 17:12:14 -0300

    Add a meta_action data to work with the post meta handler

--- post2home.js
+++ post2home.js
@@ -1,31 +1,28 @@
 jQuery(document).ready(function() {
-  
+
 	jQuery('.post2home-button').click(function() {
-		var action;
-		var update_destaque;
+		var meta_action;
 		var post_id = jQuery(this).attr('id').replace('post2home-', '');
-		if (jQuery(this).attr('checked')) {
-			action = 'destaque_add';
-		} else {
-			action = 'destaque_remove';
-		}
 		
-		update_destaque = jQuery.ajax({
+		if (jQuery(this).attr('checked'))
+			meta_action = 'update_meta';
+		else
+			meta_action = 'delete_meta';
+		
+		jQuery.ajax({
             type: 'POST',
             url: ajaxurl,
             dataType: 'html',
             data: {
 				post_id: post_id,
-				action: action
+				action: 'post2home_handle_post_meta',
+				meta_action: meta_action
             },
-            
-            complete: function() {
-                if (update_destaque.responseText != 'ok')
-                	alert('Falha ao setar destaque');
+            complete: function(jqXHR, textStatus) {
+            	if (textStatus == 'error')
+                	alert('Failed to feature your post.');
             }
         });
-		
 	});
     
-	
 });
